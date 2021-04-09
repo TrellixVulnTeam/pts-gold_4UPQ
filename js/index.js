713f1a6e33537013b33ff89d8f64d2ed0be9a296
pat-may <patchmayamba@gmail.com>
2021-04-09 20:03:42 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,165 @@
+document.querySelector('body').insertAdjacentHTML('beforebegin', `
+
+
+
+
+<div class="big-nav">
+<div class=topsmall-nav>
+    <main class="small-nav-con">
+        <img src="img/facebook.svg" alt="" class="social-icon">
+        <img src="img/instagram.svg" alt="" class="social-icon">
+        <img src="img/youtube.svg" alt="" class="social-icon">
+        <img src="img/email.svg" alt="" class="social-icon">
+    </main>
+</div>
+   <main>
+
+         <div class="nav">
+            <a class="logo-box" href="index.html">
+                <div class="logo"></div>
+                <div class="logo-text">PRO - <span>TECT</span><br>SECURITY<br>K9</div>
+           </a>
+
+
+            <div class="link-con">
+                <div class="link-box">
+                    <div class="slogan">The avant-garde of security canines worldwide</div>
+                    <span><img src="img/shopping-bag.svg"" class=" shopping-icon"></span>
+                    <div class="merchandise-text">Check out our Merchandise</div>
+                    <div class="phone-number"><span><img src="img/telephone.svg"" class=" phone-icon"></span> 0800 123
+                        4678
+                    </div>
+
+                </div>
+
+
+                <div class="nav__items">
+                    <span class="nav__list"> <a class="nav__links" href="index.html">HOME</a> </span>
+                    <span class="nav__list"> <a class="nav__links" href="about-us.html">ABOUT US</a></span>
+                    <span class="nav__list"> <a class="nav__links" href="training-services.html">TRAINING SERVICES</a></span>
+                    <span class="nav__list"> <a class="nav__links" href="security-services.html">SECURITY SERVICE</a> </span>
+                    <span class="nav__list"> <a class="nav__links" href="merchandise.html">MERCHANDISE</a> </span>
+                    <span class="nav__list"> <a class="nav__links" href="dogs-for-sale.html">DOGS FOR SELL</a> </span>
+                    <span class="nav__list"> <a class="nav__links" href="gallery.html">GALLERY</a> </span>
+                    <span class="nav__list"> <a class="nav__links" href="faq.html">FAQ</a> </span>
+                </div>
+
+                <label for="nav-trigger" class="nav-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
+            </div>
+
+
+<input type="checkbox" id="nav-trigger" name="">
+
+            <div class="slide-nav-items">   
+
+                <label for="nav-trigger" class="nav-close-icon">+</label>
+                <br><br><br><br><br><br><br><br><br><br>
+            
+   
+                <span class="slide-nav__list"><span><img src="img/house-black-silhouette-without-door.svg" alt="" class="slide-nav-icon"></span><a class="slide-nav__links" href="index.html">HOME</a> </span>
+                <span class="slide-nav__list"><span><img src="img/information.svg" alt="" class="slide-nav-icon"></span><a class="slide-nav__links" href="about-us.html">ABOUT US</a></span>
+                <span class="slide-nav__list"><span><img src="img/dog-training.svg" alt="" class="slide-nav-icon"></span> <a class="slide-nav__links" href="training-services.html">TRAINING SERVICES</a></span>
+                <span class="slide-nav__list"><span><img src="img/padlock.svg" alt="" class="slide-nav-icon"></span><a class="slide-nav__links" href="security-services.html">SECURITY SERVICE</a> </span>
+                <span class="slide-nav__list"><span><img src="img/s-bag.svg" alt="" class="slide-nav-icon"></span><a class="slide-nav__links" href="merchandise.html">MERCHANDISE</a> </span>
+                <span class="slide-nav__list"><span><img src="img/dog.svg" alt="" class="slide-nav-icon"></span><a class="slide-nav__links" href="dogs-for-sale.html">DOGS FOR SELL</a> </span>
+                <span class="slide-nav__list"><span><img src="img/gallery.svg" alt="" class="slide-nav-icon"></span><a class="slide-nav__links" href="gallery.html">GALLERY</a> </span>
+                <span class="slide-nav__list"><span><img src="img/help.svg" alt="" class="slide-nav-icon"></span><a class="slide-nav__links" href="faq.html">FAQ</a> </span>
+            
+            </div>
+    </main>
+
+   
+</div>
+
+
+`);
+
+document.querySelector('body').insertAdjacentHTML('beforeend', `
+
+<footer class="footer">
+<div class="footer-con">
+
+    <div class="footer-box1"></div>
+    <div class="footer-logo-text">PRO - <span>TECT</span><br>SECURITY<br>K9</div>
+
+    <div class="footer-box2">
+        <div class="footer-quote1">"If
+            you hear the barking of dogs or the braying of donkeys at night, seek refuge in Allah for
+            they
+            see
+            what you do not see."
+        </div>
+        <br>
+
+        <div class="footer-quote2">
+            Prophet Muhammad
+            <br>
+            Peace and blessings be upon him
+            <br>
+            Narrated by Jaabir ibn Abdullah
+        </div>
+    </div>
+
+
+
+
+
+    <div class="footer-box">
+        <div>
+            <span class="footer-title">QUICK LINKS</span>
+        </div>
+        <br>
+        <br>
+        <!-- <ul class="footer__items"> -->
+        <li class="footer__list"> <a class="footer__links" href="index.html">Home</a></li>
+        <li class="footer__list"> <a class="footer__links" href="about-us.html">About Us</a></li>
+        <li class="footer__list"> <a class="footer__links" href="training-services.html">Training Services</a></li>
+        <li class="footer__list"> <a class="footer__links" href="security-services.html">security Services</a> </li>
+        <li class="footer__list"> <a class="footer__links" href="merchandise.html">Merchandise</a> </li>
+        <li class="footer__list"> <a class="footer__links" href="dogs-for-sale.html">Dogs for Sell</a> </li>
+        <li class="footer__list"> <a class="footer__links" href="gallery.html">Gallery</a> </li>
+        <li class="footer__list"> <a class="footer__links" href="faq.html">FaQ</a> </li>
+        <!-- </ul> -->
+
+        
+    </div>
+
+    <div class="footer-box4">
+        <div>
+            <span class="footer-title">CONTACT US</span>
+        </div>
+        <br>
+
+        <span><img src="img/telephone.svg" alt="" class="footer-phone-icon"></span>
+        <span><span class="footer-number">0800 123 4567</span>
+        <br>
+        <span><img src="img/telephone.svg" alt="" class="footer-phone-icon"></span><span class="footer-number">0800 123 4567</span>
+        <br>
+        <br>
+        <div class="footer-address">
+            UK
+            Address:
+            <br>
+            119 Shelbourne Road,Tottenham, London, N17 9YD, UK
+            <br>
+            <br>
+            GM Address:
+            <br>
+            Muzdalipha Complex, Bakoteh Kanifing Municipal Area, Gambia
+            <br>
+            Working Hours:
+            <br>
+            Monday - Saturday 09:00 - 18:00
+
+        </div>
+    </div>
+</div>
+</footer>
+<div class="credits">
+prespective studio 2021 </div>
+
+`);
